363dbc1a5f49799708f547e9a2c25e5733caf0fd
Noah Calixto Moi <noahcalixtomoi@gmail.com>
2026-06-10 09:45:01 -0300

    Create ex-4.cpp

---
+++ linguagem/linguagem-c-lista-2/exercicio-4/ex-4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+double adicao(double a, double b) { return a + b; }
+double subtracao(double a, double b) { return a - b; }
+double multiplicacao(double a, double b) { return a * b; }
+
+// Função de divisão tratando o erro de divisão por zero
+double divisao(double a, double b) {
+    if (b == 0) {
+        cout << "Erro: Divisao por zero! ";
+        return 0;
+    }
+    return a / b;
+}
+
+int main() {
+    double n1, n2;
+    cout << "Digite dois numeros: ";
+    cin >> n1 >> n2;
+
+    cout << "Adicao: " << adicao(n1, n2) << endl;
+    cout << "Subtracao: " << subtracao(n1, n2) << endl;
+    cout << "Multiplicacao: " << multiplicacao(n1, n2) << endl;
+    cout << "Divisao: " << divisao(n1, n2) << endl;
+
+    return 0;
+}
