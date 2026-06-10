047a99b6dbce0b856d8a0fc3a58300503bbcad42
Noah Calixto Moi <noahcalixtomoi@gmail.com>
2026-06-10 09:44:20 -0300

    Create ex-2.cpp

---
+++ linguagem/linguagem-c-lista-2/exercicio-2/ex-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Função que calcula a potência usando um laço de repetição
+long long calcularPotencia(int base, int expoente) {
+    long long resultado = 1;
+    for (int i = 0; i < expoente; i++) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
+int main() {
+    int base, expoente;
+
+    cout << "Digite a base: ";
+    cin >> base;
+    cout << "Digite o expoente: ";
+    cin >> expoente;
+
+    cout << base << " elevado a " << expoente << " eh: " 
+         << calcularPotencia(base, expoente) << endl;
+
+    return 0;
+}
