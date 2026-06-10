bb067b798273b666669f2f037c8a3ae0b2e0b23f
Noah Calixto Moi <noahcalixtomoi@gmail.com>
2026-06-10 09:46:57 -0300

    Create ex-9.cpp

---
+++ linguagem/linguagem-c-lista-2/exercicio-9/ex-9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Função que altera a string substituindo vogais
+string ocultarVogais(string palavra) {
+    for (int i = 0; i < palavra.length(); i++) {
+        char c = tolower(palavra[i]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            palavra[i] = '*';
+        }
+    }
+    return palavra;
+}
+
+int main() {
+    string palavra;
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+
+    cout << "Palavra modificada: " << ocultarVogais(palavra) << endl;
+    return 0;
+}
