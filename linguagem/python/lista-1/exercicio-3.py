c0cf50173203406d2a1b70ca58b1b84244a19de3
Noah Calixto Moi <noahcalixtomoi@gmail.com>
2026-08-27 19:26:48 -0300

    Add counting script from 1 to user-defined number
    This script prompts the user for a number and counts from 1 to that number.

---
+++ linguagem/python/lista-1/exercicio-3.py
@@ -0,0 +1,13 @@
+"""
+Autor: Miguel Silva Sacheto e Noah Calixto moi
+Data: agosto/2026
+Descrição: vc coloca um numero e ele conta de 1 ate esse numero
+"""
+#defino soma pra 1 para depois utilizala para contar
+soma = 1
+#coloco int para so aceitar numero inteiro 
+numero  = int(input("digite o numero desejado:"))
+#enquanto a soma for menor que o numero dado ele vai continuar rodando o codigo
+while (soma<=numero):
+    print (soma)
+    soma += 1
